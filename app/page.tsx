e8032fdec88372b347118b617227b6135adebdae
Dinmukhamed Junussov <daurenzhunussov@Daurens-MacBook-Air.local>
2025-04-29 20:41:09 +0500

    changed text

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Понимайте Результаты Ваших Анализов Крови
+                  Расшифровка Результатов Ваших Анализов Крови
                 </h1>
                 <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
                   Получите персонализированную интерпретацию и рекомендации по здоровью на основе ваших лабораторных
